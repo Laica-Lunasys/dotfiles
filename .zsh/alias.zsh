0d5b29a22939593129a6d885e60859b2842fe1c8
Laica-Lunasys <laica.lunasys@gmail.com>
2017-09-30 15:59:10 +0900

    Fixed Rebirth step

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,2 +1,2 @@
-alias rebirth='echo Reincarnate zsh... ; source ~/.zshrc'
+alias rebirth='echo Reincarnate zsh... ; rm -rfv ~/.antigen/ ; [ ! -d ~/.antigen ] && mkdir -p ~/.antigen && curl -L git.io/antigen > ~/.antigen/antigen.zsh; source ~/.zshrc'
 alias nconsole='tmux new-s -s Console'
