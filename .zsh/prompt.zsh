cb652e664180c6896421a5151b148345dd59151f
Laica-Lunasys <laica.lunasys@gmail.com>
2018-03-09 09:20:45 +0900

    Solid Prompt theme

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -31,19 +31,20 @@ PROMPT=""
 RESET=$reset_color
 
 # user@hostname
-PROMPT+=$'%{\e[38;5;255;48;5;236m%} %n@%m '$'%{\e[38;5;236m%}'
-#%{\e[38;5;255m%}
+PROMPT+=$'%{\e[38;5;255;48;5;236m%}  %n@%m  '$'%{\e[38;5;236m%}'
+#%{\e[38;5;255m%}   
 
 # directory
-PROMPT+=$'%{\e[48;5;39m%}'"▍"$'%{\e[38;5;0m%} %~ '$'%{\e[38;5;39m%}'
+PROMPT+=$'%{\e[48;5;39m%}'"▍"$'%{\e[38;5;0m%}  %~  '$'%{\e[38;5;39m%}'
 
 PROMPT+="\$(
 if [[ -n \"\${vcs_info_msg_0_}\" ]]; then
-    echo $'%{\e[48;5;148m%}'"▍"\"%{\e[38;5;0m%} ⤭ \${vcs_info_msg_0_}\${vcs_info_msg_1_}\"$'%{\e[m\e[38;5;148m%}'"▍"
+    echo $'%{\e[48;5;148m%}'"▍"\"%{\e[38;5;0m%} ⤭  \${vcs_info_msg_0_}\${vcs_info_msg_1_} \"$'%{\e[m\e[38;5;148m%}'
   else 
-    echo $'%{\e[m%}%{\e[38;5;39m%}'"▍"
+    echo $'%{\e[m%}%{\e[38;5;39m%}'
 fi)"
 
+PROMPT+="${reset_color}"
 # PROMPT+=$'%{\e[48;5;148m%}'"▍"
 # PROMPT+=$'%{\e[38;5;0m%}${vcs_info_msg_0_}${vcs_info_msg_1_}%}'
 # PROMPT+=$'%{\e[m\e[38;5;148m%}'"▍"
