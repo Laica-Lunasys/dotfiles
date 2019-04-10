54d453734c13a429eced8d32cad32a533cb0e068
Laica-Lunasys <laica.lunasys@gmail.com>
2019-04-10 14:41:11 +0900

    Colorize Setup message

--- setup/init.sh
+++ setup/init.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 # Prefix
-PREFIX="\e[1;32m==>\e[00m"
-RPREFIX="\e[1;35m==>\e[00m"
-EPREFIX="\e[1;31m!!!\e[00m"
+_info_prefix=$(printf "\e[1;34m::\e[m")
+_notify_prefix=$(printf "\e[1;32m==>\e[m")
+_error_prefix=$(printf "\e[1;31m!!\e[m")
 #---------------------------------------
 
-echo -e "$PREFIX Setup zsh..."
+echo "$_info_prefix Setup zsh..."
 # zplug: [ ! -d ~/.zplug ] && mkdir -p ~/.zplug && curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 [ ! -d ~/.antigen ] && mkdir -p ~/.antigen && curl -L git.io/antigen > ~/.antigen/antigen.zsh
 
-echo -e "$PREFIX Setup tpm..."
+echo "$_info_prefix Setup tpm..."
 [ ! -d ~/.tmux/plugins/tpm ] && mkdir -p ~/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo -e "$PREFIX Setup vim-plug..."
+echo "$_info_prefix Setup vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo -e "$RPREFIX Executing: 'vim -c :PlugInstall'"
+echo "$_notify_prefix Executing: 'vim -c :PlugInstall'"
 vim -c ':PlugInstall' -c ':q' -c ':q'
 
-#echo -e "$PREFIX Setup vim Vundle..."
+#echo "$_info_prefix Setup vim Vundle..."
 #[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 
-#echo -e "$RPREFIX Executing: 'vim -c :PluginInstall'"
+#echo "$_notify_prefix Executing: 'vim -c :PluginInstall'"
 #vim -c ':PluginInstall' -c ':q' -c ':q'
 
-echo -e "$PREFIX vim / Vundle Ready."
+echo "$_info_prefix vim / Vundle Ready."
