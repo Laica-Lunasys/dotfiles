ce9580e44cfed432850b1ba9d5b7feac20a6f71a
Laica Lunasys <laica.lunasys@gmail.com>
2019-04-15 23:36:51 +0900

    Add termsupport

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -6,6 +6,8 @@ zplug "plugins/docker", from:oh-my-zsh
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
+zplug "robbyrussell/oh-my-zsh", use:lib/termsupport.zsh, as:plugin
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
