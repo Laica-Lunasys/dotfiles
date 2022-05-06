273a20d55cb26c09fb6f32cfdbafb51ae082f45c
Laica Lunasys <laica.lunasys@gmail.com>
2022-05-06 18:24:19 +0900

    Remove enhancd

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -4,4 +4,3 @@ zinit light romkatv/powerlevel10k
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
-zinit light b4b4r07/enhancd
