da8339f40ba461e5d4a2c1ff839628071ce3e297
Laica Lunasys <laica.lunasys@gmail.com>
2022-05-23 11:33:49 +0900

    Add oh-my-zsh

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -4,3 +4,5 @@ zinit light romkatv/powerlevel10k
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
+
+zinit light ohmyzsh/ohmyzsh
