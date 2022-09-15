d7e35c6bd91a58363e0347ddda6dab2e28a53963
Laica Lunasys <laica.lunasys@gmail.com>
2022-09-15 17:21:58 +0900

    Fix loading order of ohmyzsh

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -1,8 +1,9 @@
 zinit ice depth"1"
 zinit light romkatv/powerlevel10k
 
+# ohmyzsh
+zinit light ohmyzsh/ohmyzsh
+
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
-
-zinit light ohmyzsh/ohmyzsh
