fdcd2901d01ae3b06c0f4a289b3c7117d392c7e0
Laica Lunasys <laica.lunasys@gmail.com>
2022-11-30 18:44:55 +0900

    Switch to fast-syntax-highlighting

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -4,6 +4,8 @@ zinit light romkatv/powerlevel10k
 # ohmyzsh
 zinit light ohmyzsh/ohmyzsh
 
-zinit light zsh-users/zsh-syntax-highlighting
+#zinit ice wait'!0'; zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
+#zinit ice wait'!0'; zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
