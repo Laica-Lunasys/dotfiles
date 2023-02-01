21202357d06f072ccfc2176e5568cfff3deb67c0
Laica Lunasys <laica.lunasys@gmail.com>
2023-02-01 18:21:10 +0900

    Fix lspsaga

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -1,7 +1,7 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.init_lsp_saga {
+saga.setup {
     server_filetype_map = {
         typescript = 'typescript'
     }
