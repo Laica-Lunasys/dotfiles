b2bd6b5b1622739471565a6d12ca8ffb5fdc9e93
Laica Lunasys <laica.lunasys@gmail.com>
2024-11-16 15:18:44 +0900

    Improve comment / file cmds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,10 @@
 require("config.lazy")
+
+-- --------------------------------------------
+-- Load config
+-- --------------------------------------------
+-- DO NOT UNCOMMENT HERE
+-- LazyVim will load those files automatically.
+-- require("config.autocmds")
+-- require("config.keymaps")
+-- require("config.options.lua")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,8 +3,19 @@ vim.keymap.set("", "<C-e>", function()
 	require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
 end)
 
-vim.keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Tab
+-- vim.keymap.set("", "<tab>", ":tabnext<Return>", opts)
+-- vim.keymap.set("", "<s-tab>", ":tabprev<Return>", opts)
 
-vim.keymap.set("n", "<C-]>", vim.lsp.buf.code_action, bufopts)
-vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, bufopts)
+-- LSP
+vim.keymap.set("", "<C-]>", vim.lsp.buf.code_action, bufopts)
+vim.keymap.set("i", "<C-]>", vim.lsp.buf.code_action, bufopts)
+vim.keymap.set("", "<C-f>", vim.lsp.buf.format, bufopts)
+vim.keymap.set("i", "<C-f>", vim.lsp.buf.format, bufopts)
+
+-- Code edit
+local doComment = function()
+	require("Comment.api").toggle.linewise.current()
+end
+vim.keymap.set("", "<C-_>", doComment, { noremap = true, silent = true })
+vim.keymap.set("i", "<C-_>", doComment, { noremap = true, silent = true })
