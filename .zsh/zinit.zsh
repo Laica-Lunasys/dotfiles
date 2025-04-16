dc79801cf82fd2e6c31fc2f8f2500a65f54e1082
Laica Lunasys <laica.lunasys@gmail.com>
2025-04-16 17:15:32 +0900

    Remove omz

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -1,9 +1,6 @@
 zinit ice depth"1"
 zinit light romkatv/powerlevel10k
 
-# ohmyzsh
-# zinit light ohmyzsh/ohmyzsh
-
 #zinit ice wait'!0'; zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zdharma-continuum/fast-syntax-highlighting
 #zinit ice wait'!0'; zinit light zsh-users/zsh-syntax-highlighting
