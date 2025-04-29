164407dec3e359514a088149b13e34e3d0143570
Laica Lunasys <laica.lunasys@gmail.com>
2025-04-29 23:56:54 +0900

    Remove unused alias file

--- .zsh/alias.zsh
+++
@@ -1,2 +0,0 @@
-alias rebirth='echo Reincarnate zsh... ; rm -rfv ~/.antigen/ ; [ ! -d ~/.antigen ] && mkdir -p ~/.antigen && curl -L git.io/antigen > ~/.antigen/antigen.zsh; source ~/.zshrc'
-alias nconsole='tmux new-s -s Console'
