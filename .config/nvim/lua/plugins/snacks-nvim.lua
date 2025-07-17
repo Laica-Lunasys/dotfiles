89bd61a0b6471639a35fc99e4132558d164f1a32
Laica Lunasys <laica.lunasys@gmail.com>
2025-07-17 16:46:09 +0900

    Disable scroll animation

--- .config/nvim/lua/plugins/snacks-nvim.lua
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -1,30 +1,38 @@
 return {
-    {
-        "folke/snacks.nvim",
-        opts = {
-            dashboard = {
-                preset = {
-                    header = [[ WELCOME ]],
-                    -- stylua: ignore
-                    ---@type snacks.dashboard.Item[]
-                    keys = {
-                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-                        { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                    },
-                },
-            },
-            sections = {
-                { section = "header" },
-                { section = "keys",   gap = 1, padding = 1 },
-                { section = "startup" },
-            },
-        },
-    }
+	{
+		"snacks.nvim",
+		opts = {
+			dashboard = {
+				preset = {
+					header = [[ WELCOME ]],
+					-- stylua: ignore
+					---@type snacks.dashboard.Item[]
+					keys = {
+						{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+						{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+						{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+						{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+						{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{ icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+						{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					},
+				},
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys",   gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+			indent = { enabled = true },
+			input = { enabled = true },
+			notifier = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = false },
+			statuscolumn = { enabled = false }, -- we set this in options.lua
+			toggle = { map = LazyVim.safe_keymap_set },
+			words = { enabled = true },
+		},
+	}
 }
