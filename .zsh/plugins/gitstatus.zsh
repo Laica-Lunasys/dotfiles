575addc5c61edd19eff1f7f0e7a27c3b1afe251b
Laica Lunasys <laica.lunasys@gmail.com>
2025-07-17 16:53:31 +0900

    [Experimental] Async prompt update for large git repository

--- .zsh/plugins/gitstatus.zsh
+++ .zsh/plugins/gitstatus.zsh
@@ -26,22 +26,22 @@
 function gitstatus_prompt_update() {
   export PROMPT_VCS_STATUS=""
   emulate -L zsh
-  typeset -g  GITSTATUS_PROMPT=''
+  typeset -g GITSTATUS_PROMPT=''
   typeset -gi GITSTATUS_PROMPT_LEN=0
 
   # Call gitstatus_query synchronously. Note that gitstatus_query can also be called
   # asynchronously; see documentation in gitstatus.plugin.zsh.
-  gitstatus_query 'MY'                  || return 1  # error
-  [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0  # not a git repo
+  gitstatus_query 'MY' || return 1                  # error
+  [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0 # not a git repo
 
-  local      clean='%76F'   # green foreground
-  local   modified='%178F'  # yellow foreground
-  local  untracked='%39F'   # blue foreground
-  local conflicted='%196F'  # red foreground
+  local clean='%76F'       # green foreground
+  local modified='%178F'   # yellow foreground
+  local untracked='%39F'   # blue foreground
+  local conflicted='%196F' # red foreground
 
   local p
 
-  local where  # branch name, tag or commit
+  local where # branch name, tag or commit
   if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
     where=$VCS_STATUS_LOCAL_BRANCH
   elif [[ -n $VCS_STATUS_TAG ]]; then
@@ -49,40 +49,42 @@ function gitstatus_prompt_update() {
     where=$VCS_STATUS_TAG
   else
     p+='%f@'
-    where=${VCS_STATUS_COMMIT[1,8]}
+    where=${VCS_STATUS_COMMIT[1, 8]}
   fi
 
-  (( $#where > 32 )) && where[13,-13]="…"  # truncate long branch names and tags
-  p+="${clean}${where//\%/%%}"             # escape %
+  (($#where > 32)) && where[13, -13]="…" # truncate long branch names and tags
+  p+="${clean}${where//\%/%%}"           # escape %
 
   # ⇣42 if behind the remote.
-  (( VCS_STATUS_COMMITS_BEHIND )) && p+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+  ((VCS_STATUS_COMMITS_BEHIND)) && p+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
   # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
-  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && p+=" "
-  (( VCS_STATUS_COMMITS_AHEAD  )) && p+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+  ((VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND)) && p+=" "
+  ((VCS_STATUS_COMMITS_AHEAD)) && p+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
   # ⇠42 if behind the push remote.
-  (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && p+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
-  (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && p+=" "
+  ((VCS_STATUS_PUSH_COMMITS_BEHIND)) && p+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
+  ((VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND)) && p+=" "
   # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
-  (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && p+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
+  ((VCS_STATUS_PUSH_COMMITS_AHEAD)) && p+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
   # *42 if have stashes.
-  (( VCS_STATUS_STASHES        )) && p+=" ${clean}*${VCS_STATUS_STASHES}"
+  ((VCS_STATUS_STASHES)) && p+=" ${clean}*${VCS_STATUS_STASHES}"
   # 'merge' if the repo is in an unusual state.
-  [[ -n $VCS_STATUS_ACTION     ]] && p+=" ${conflicted}${VCS_STATUS_ACTION}"
+  [[ -n $VCS_STATUS_ACTION ]] && p+=" ${conflicted}${VCS_STATUS_ACTION}"
   # ~42 if have merge conflicts.
-  (( VCS_STATUS_NUM_CONFLICTED )) && p+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
+  ((VCS_STATUS_NUM_CONFLICTED)) && p+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
   # +42 if have staged changes.
-  (( VCS_STATUS_NUM_STAGED     )) && p+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
+  ((VCS_STATUS_NUM_STAGED)) && p+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
   # !42 if have unstaged changes.
-  (( VCS_STATUS_NUM_UNSTAGED   )) && p+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+  ((VCS_STATUS_NUM_UNSTAGED)) && p+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
   # ?42 if have untracked files. It's really a question mark, your font isn't broken.
-  (( VCS_STATUS_NUM_UNTRACKED  )) && p+=" ${untracked}?${VCS_STATUS_NUM_UNTRACKED}"
+  ((VCS_STATUS_NUM_UNTRACKED)) && p+=" ${untracked}?${VCS_STATUS_NUM_UNTRACKED}"
 
-  p+='%f'  # reset color
+  p+='%f' # reset color
   export PROMPT_VCS_STATUS=$(print -P "${p}")
 
   # The length of GITSTATUS_PROMPT after removing %f and %F.
-  GITSTATUS_PROMPT_LEN="${(m)#${${GITSTATUS_PROMPT//\%\%/x}//\%(f|<->F)}}"
+  # GITSTATUS_PROMPT_LEN="${(m)#${${GITSTATUS_PROMPT//\%\%/x}//\%(f|<->F)}}"
+
+  print "${PROMPT_VCS_STATUS}"
 }
 
 # Start gitstatusd instance with name "MY". The same name is passed to
@@ -90,8 +92,47 @@ function gitstatus_prompt_update() {
 # enable staged, unstaged, conflicted and untracked counters.
 gitstatus_stop 'MY' && gitstatus_start -s -1 -u -1 -c -1 -d -1 'MY'
 
+# add-zsh-hook precmd gitstatus_prompt_update
+
+gitstatus_hook() {
+  export PROMPT_VCS_STATUS="$3"
+  zle reset-prompt
+}
+
+gitstatus_update_prompt() {
+  # export PROMPT_VCS_STATUS="A"
+  gitstatus_prompt_update
+
+  # print ${PROMPT_VCS_STATUS}
+  # echo "OK"
+}
+
+function update_dir() {
+  if [[ "$PWD" != "$1" ]]; then
+    cd "$1"
+  fi
+
+  gitstatus_prompt_update
+}
+
+async_init
+async_start_worker gitstatus_async
+async_register_callback gitstatus_async gitstatus_hook
+# async_job gitstatus_async gitstatus_update_prompt
+async_worker_eval gitstatus_async update_dir $PWD
+
+gitstatus_precmd() {
+  async_worker_eval gitstatus_async update_dir $PWD
+  # async_start_worker gitstatus_async
+  # async_register_callback gitstatus_async gitstatus_hook
+  # async_job gitstatus_async gitstatus_update_prompt
+}
+
 # On every prompt, fetch git status and set GITSTATUS_PROMPT.
-add-zsh-hook precmd gitstatus_prompt_update
+# add-zsh-hook precmd gitstatus_precmd
+precmd() {
+  gitstatus_precmd
+}
 
 # Enable/disable the right prompt options.
 setopt no_prompt_bang prompt_percent prompt_subst
